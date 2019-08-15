54c95015c0dc5adb981277013faab3f9380c8595
Larissa <larissacorraide@hotmail.com>
2019-08-14 19:15:40 -0500

    Estimating dew yield/night

--- Mapping.py
+++ Mapping.py
@@ -1,10 +1,11 @@
 #Importing libraries ---------------------------------------------------------------------------------------------------
 
 import pandas as pd
-
+import numpy as np
+import math
+import datetime
 
 # defining variables ---------------------------------------------------------------------------------------------------
-
 
 temperature_label = "Temperatura"
 humidity_label = "Umidade"
@@ -12,19 +13,52 @@ dew_label = "Ponto_orvalho"
 wind_label="Vento_velocidade"
 pressure_label = "Pressao"
 name_label ="nome"
+estimation_label = "Estimation"
 
-date_label ="datetime"
+datetime_label ="datetime"
 
 station_label = "cod"
 latitude_label = "lat"
 altitude_label = "alt"
 longitude_label ="lon"
 
-select = [temperature_label,humidity_label,dew_label,wind_label,date_label,
+select = [temperature_label,humidity_label,dew_label,wind_label,datetime_label,
           name_label,latitude_label,longitude_label,altitude_label]
 
-# Reading main data-----------------------------------------------------------------------------------------------------
-df = pd.read_csv('all_data_all_weather_stations.csv')
+# functions ------------------------------------------------------------------------------------------------------------
 
+def dew_estimation_function (row):
+
+    try:
+        H = row[altitude_label]/1000
+        Ta= row[temperature_label]
+        Td= row[dew_label]
+        L = row [latitude_label]
+        C=0
+        w = row[wind_label]
+
+        dew_estimation = np.round((((0.37 * (1 + (0.204323 * H) - (0.0238893 * (H ** 2)) - (18.0123 -
+                        (1.04963 * H) + (0.21891 * (H ** 2))) * (10 ** (-3)) * Td) * (((Td + 273.15) / 285) ** 4)
+                        *((1 - (C / 8)))) + (0.06 * (Td - Ta) * (1 + (100 * (1 - math.exp(-(w / 4.4) ** 20)))))/12))*7,4)
+
+
+        if ((dew_estimation < 0) or (w >= 4.4)):
+            dew_estimation = 0
+
+        return dew_estimation
+
+    except:
+        print("Erro")
+
+
+# Reading main data and selecting---------------------------------------------------------------------------------------
+df = pd.read_csv('all_data_all_weather_stations.csv')
 df = df[select]
-print(df)
+
+df[datetime_label] = pd.to_datetime(df[datetime_label])
+
+#df = df[df[datetime_label].apply(lambda d: d.hour == 6)] #Estimation using the 6am data
+
+df[estimation_label] = df[df[datetime_label].apply(lambda d: d.hour == 6)].apply(dew_estimation_function, axis=1)
+
+df.to_csv('teste.csv',encoding="utf-8")
