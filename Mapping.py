87674eb20af0871047a31560225d795b8668a9fd
Larissa <larissacorraide@hotmail.com>
2019-08-19 17:34:44 -0500

    Average dew yield by weather station and season

--- Mapping.py
+++ Mapping.py
@@ -21,6 +21,7 @@ station_label = "cod"
 latitude_label = "lat"
 altitude_label = "alt"
 longitude_label ="lon"
+season_label = "Season"
 
 select = [temperature_label,humidity_label,dew_label,wind_label,datetime_label,
           name_label,latitude_label,longitude_label,altitude_label]
@@ -50,20 +51,49 @@ def dew_estimation_function (row):
     except:
         print("Erro")
 
+def season(df):
+    if ((df[datetime_label].month == 12) or (df[datetime_label].month == 1) or
+        (df[datetime_label].month == 2)):
+        season = 1
+
+    elif ((df[datetime_label].month == 3) or (df[datetime_label].month == 4) or
+    (df[datetime_label].month == 5)):
+        season = 2
+
+    elif ((df[datetime_label].month == 6) or (df[datetime_label].month == 7) or
+          (df[datetime_label].month == 8)):
+        season = 3
+
+    elif ((df[datetime_label].month == 9 ) or (df[datetime_label].month == 10) or
+          (df[datetime_label].month == 11)):
+        season = 4
+
+    return season
+
 
 # Reading main data and selecting---------------------------------------------------------------------------------------
-df = pd.read_csv('all_data_all_weather_stations.csv')
-df = df[select]
-
-df[datetime_label] = pd.to_datetime(df[datetime_label])
+df = pd.read_csv('all_data_all_weather_stations.csv') # reading data
+#df = df.sample(n=1000)
+df = df[select] # selecting variables
+df[datetime_label] = pd.to_datetime(df[datetime_label]) # converting to datetime
 
 df = df[df[datetime_label].apply(lambda d: d.hour == 6)] # Selecting data 6am
-
 df[estimation_label] = df.apply(dew_estimation_function, axis=1) #Estimating dew yield
+df[season_label] = df.apply(season, axis=1) #Classifying seasons
 
-df1 = df.groupby([df[name_label], df[datetime_label].dt.month])[estimation_label].mean() #Dew yield mean by weather station
+df1 = df.groupby([df[name_label], df[season_label]])[estimation_label].mean().reset_index().rename(
+    columns={name_label:name_label,season_label : season_label, estimation_label:estimation_label}) # Average Dew yield mean by weather station and season
 
-df1.to_csv('teste.csv',encoding="utf-8") #Saving data
+df1.set_index(name_label, inplace = True) #selecting index
+df1[estimation_label] = round(df1[estimation_label],3)
+df1.to_csv('dew_estimation.csv', encoding="utf-8") #Saving data
 
+df_season1 = df1[df1[season_label].apply(lambda d: d == 1)] # Filtering data by season 1
+df_season2 = df1[df1[season_label].apply(lambda d: d == 2)] # Filtering data by season 2
+df_season3 = df1[df1[season_label].apply(lambda d: d == 3)] # Filtering data by season 3
+df_season4 = df1[df1[season_label].apply(lambda d: d == 4)] # Filtering data by season 4
 
-#df[datetime_label].dt.month
+df_season1.to_csv('df_season1.csv',encoding="utf-8") #Saving data seasons 1
+df_season2.to_csv('df_season2.csv',encoding="utf-8") #Saving data seasons 2
+df_season3.to_csv('df_season3.csv',encoding="utf-8") #Saving data seasons 3
+df_season4.to_csv('df_season4.csv',encoding="utf-8") #Saving data seasons 4
